c10d0731b6b703e998362bb8235af700bef1243d
Akshat <akakkar_be23@thapar.edu>
2025-11-01 23:50:21 +0530

    NewsletterBox.jsx

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Subscribe Now & get 20% off</p>
+        <p className='text-gray-400 mt-3'>
+            Sign up for our newsletter to receive the latest updates and exclusive offers.
+        </p>
+        <form>
+            <input type="email" placeholder='Enter your email' className='w-full sm:flex-1 outline-none' required />
+            <button className='bg-black text-white text-xs px-10 py-4' type='submit'>SUBSCRIBE</button>
+        </form>
+    </div>
+  )
+}
+
+export default NewsletterBox
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <BestSeller />
+      <OurPolicy />
+      <NewsletterBox />
       <h1 className='flex items-center justify-center'>Made By <span className='text-red-600'>&#9829;</span> with Akshat</h1>
     </div>
   )
