93eaa96fa90d87fae1bcf9e29ddecbf6a7a2a4de
Akshat <akakkar_be23@thapar.edu>
2026-01-11 17:21:41 +0530

    written some backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+
+
+//route for user login
+const loginUser = async (req,res) =>{
+
+}
+
+
+//route for user registration
+const registerUser = async (req,res) =>{
+   
+
+}
+//route for admin login
+const adminLogin = async (req,res) =>{
+
+
+}
+
+export {loginUser, registerUser, adminLogin}
