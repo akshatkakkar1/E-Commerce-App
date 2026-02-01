a648fb59e3b5ed9e7227e0957d4e9a28a9054688
Akshat <akakkar_be23@thapar.edu>
2026-02-01 17:41:01 +0530

    2nd

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -44,6 +44,21 @@ const ShopContextProvider = (props) =>{
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if(token){
+            try{
+
+                await axios.post(backendUrl + '/api/cart/add',{itemId,size},{
+                    headers:{
+                       token
+                    }
+                });
+            }catch(error){
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
+
     }
 
 
@@ -71,6 +86,20 @@ const ShopContextProvider = (props) =>{
         cartData[itemId][size]=quantity;
 
         setCartItems(cartData);
+
+        if(token){
+            try{    
+                await axios.post(backendUrl + '/api/cart/update',{itemId,size,quantity},{
+                    headers:{
+                       token
+                    }
+                });
+
+            }catch(error){
+                console.log(error);
+                toast.error(error.message);
+            }
+        }
      }
     
 
@@ -107,10 +136,35 @@ const ShopContextProvider = (props) =>{
         }
      }
 
+     const getUserCart = async(token)=>{
+        try{
+
+            const response = await axios.post(backendUrl + '/api/cart/get',{},{
+                headers:{
+                   token
+                }
+            });
+
+            if(response.data.success){
+                setCartItems(response.data.cartData);
+            }
+        }
+        catch(error){
+            console.log(error);
+            toast.error(error.message);
+        }
+     }
+
      useEffect(()=>{
         getProductsData();
      },[])
 
+     useEffect(()=>{
+        if(!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'));
+            getUserCart(localStorage.getItem('token'));
+        }
+     },[])
      
     const value = {
         products,
